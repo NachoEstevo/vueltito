92a90273f64c3cb11e775fc25439d6fa8a7f3041
NachoEstevo <nachoestevo@gmail.com>
2026-06-06 15:07:48 -0300

    Wire brand backgrounds + official isotipo into landing

--- web/script.js
+++ web/script.js
@@ -2,17 +2,6 @@
 (function () {
   'use strict';
 
-  // ---- inject brand mark into every .brand-mark ----
-  var markSVG =
-    '<svg class="mark" viewBox="0 0 40 40" aria-hidden="true">' +
-    '<path d="M32.5 20a12.5 12.5 0 1 0-3.4 8.6" fill="none" stroke="url(#brandGrad)" stroke-width="3.4" stroke-linecap="round"/>' +
-    '<path d="M26.4 17.6l2.7 3.6 3.8-2.2" fill="none" stroke="#7AC943" stroke-width="3.4" stroke-linecap="round" stroke-linejoin="round"/>' +
-    '<path d="M20 29v-7.4M20 21.6c0-3.4-2.9-5.8-6.3-5.7.1 3.4 2.9 5.8 6.3 5.7Zm0-.5c0-3.1 2.6-5.2 5.8-5.1.1 3.2-2.6 5.2-5.8 5.1Z" fill="#7AC943"/>' +
-    '</svg>';
-  document.querySelectorAll('.brand-mark').forEach(function (el) {
-    el.innerHTML = markSVG;
-  });
-
   // ---- nav scroll shadow ----
   var nav = document.getElementById('nav');
   var onScroll = function () {
